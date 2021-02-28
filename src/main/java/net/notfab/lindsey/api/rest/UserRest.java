ac9e153c085ec37abc34008871d529e8fd558572
Fabricio Winter <git@notfab.net>
2021-02-28 17:36:55 -0300

    Add user rest

---
+++ src/main/java/net/notfab/lindsey/api/rest/UserRest.java
@@ -0,0 +1,24 @@
+package net.notfab.lindsey.api.rest;
+
+import net.notfab.lindsey.api.advice.security.SessionProvider;
+import net.notfab.lindsey.api.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserRest {
+
+    private final SessionProvider sessionProvider;
+
+    public UserRest(SessionProvider sessionProvider) {
+        this.sessionProvider = sessionProvider;
+    }
+
+    @GetMapping("@me")
+    public User getSelf() {
+        return this.sessionProvider.getUser();
+    }
+
+}
