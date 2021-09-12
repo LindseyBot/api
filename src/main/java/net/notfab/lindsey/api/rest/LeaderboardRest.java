153ab4ccd595f165ac0c90ce3fdbed00f5bca6cd
Fabricio Winter <git@notfab.net>
2021-09-12 15:56:38 -0300

    Update libraries

--- src/main/java/net/notfab/lindsey/api/rest/LeaderboardRest.java
+++ src/main/java/net/notfab/lindsey/api/rest/LeaderboardRest.java
@@ -4,14 +4,11 @@ import net.notfab.lindsey.api.advice.paging.PagedResponse;
 import net.notfab.lindsey.api.advice.paging.Paginator;
 import net.notfab.lindsey.api.models.DiscordUser;
 import net.notfab.lindsey.api.models.LeaderboardEntry;
-import net.notfab.lindsey.api.repositories.sql.LeaderboardRepository;
-import net.notfab.lindsey.api.repositories.sql.UserPrivacyRepository;
 import net.notfab.lindsey.api.repositories.sql.UserProfileRepository;
-import net.notfab.lindsey.shared.entities.leaderboard.Leaderboard;
 import net.notfab.lindsey.shared.entities.profile.UserProfile;
-import net.notfab.lindsey.shared.entities.profile.user.UserPrivacy;
 import net.notfab.lindsey.shared.enums.LeaderboardType;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,57 +20,47 @@ import java.util.stream.Collectors;
 @RequestMapping("leaderboards")
 public class LeaderboardRest {
 
-    private final LeaderboardRepository repository;
-    private final UserProfileRepository userProfileRepository;
-    private final UserPrivacyRepository userPrivacyRepository;
+    private final UserProfileRepository profiles;
 
-    public LeaderboardRest(LeaderboardRepository repository,
-                           UserProfileRepository userProfileRepository,
-                           UserPrivacyRepository userPrivacyRepository) {
-        this.repository = repository;
-        this.userProfileRepository = userProfileRepository;
-        this.userPrivacyRepository = userPrivacyRepository;
+    public LeaderboardRest(UserProfileRepository profiles) {
+        this.profiles = profiles;
     }
 
     @GetMapping
     public PagedResponse<LeaderboardEntry> findAll(Paginator paginator, @RequestParam("type") LeaderboardType type) {
-        Page<Leaderboard> page = repository.findAllByTypeOrderByCountDesc(type, paginator.toPageable());
+        Sort sort = switch (type) {
+            case COOKIES -> Sort.by(Sort.Direction.DESC, "cookies");
+            case SLOT_WINS -> Sort.by(Sort.Direction.DESC, "slotWins");
+        };
+        Page<UserProfile> page = this.profiles.findAll(paginator.toPageable(sort));
         PagedResponse<LeaderboardEntry> response = new PagedResponse<>();
         response.setLimit(page.getPageable().getPageSize());
         response.setPage(page.getNumber());
         response.setLast(page.isLast());
-        response.setItems(page.stream().map(this::toEntry).collect(Collectors.toList()));
+        response.setItems(page.stream().map((profile) -> this.toEntry(profile, type)).collect(Collectors.toList()));
         return response;
     }
 
-    private LeaderboardEntry toEntry(Leaderboard lb) {
+    private LeaderboardEntry toEntry(UserProfile profile, LeaderboardType type) {
         LeaderboardEntry entry = new LeaderboardEntry();
-        entry.setId(lb.getId());
-        entry.setCount(lb.getCount());
-        entry.setType(lb.getType());
-
+        entry.setId(profile.getUser());
+        entry.setCount(switch (type) {
+            case COOKIES -> profile.getCookies();
+            case SLOT_WINS -> profile.getSlotWins();
+        });
+        entry.setType(type);
         DiscordUser user = new DiscordUser();
-        user.setId(lb.getUser());
-        entry.setUser(user);
-
-        UserPrivacy privacy = userPrivacyRepository.findById(lb.getUser())
-                .orElse(new UserPrivacy());
-        if (privacy.isAnonymousInLeaderboards()) {
+        user.setId(profile.getUser());
+        if (profile.getName() == null) {
             user.setUsername("Anonymous");
             user.setDiscriminator("0000");
         } else {
-            UserProfile profile = this.userProfileRepository.findById(lb.getUser())
-                    .orElse(new UserProfile());
-            if (profile.getName() == null) {
-                user.setUsername("Ghost");
-                user.setDiscriminator("0000");
-            } else {
-                String name = profile.getName().split("#")[0];
-                String discrim = profile.getName().split("#")[1];
-                user.setUsername(name);
-                user.setDiscriminator(discrim);
-            }
+            String name = profile.getName().split("#")[0];
+            String discriminator = profile.getName().split("#")[1];
+            user.setUsername(name);
+            user.setDiscriminator(discriminator);
         }
+        entry.setUser(user);
         return entry;
     }
 
